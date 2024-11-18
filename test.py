8ba0fa151edd02ceaac8180aeb38bafb1da41dfb
Flávio Alexandre Orrico Severiano <flavio.severiano@alaresinternet.com.br>
2024-11-18 08:30:32 -0300

    adding UNM user extractor

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 import pyautogui
 
-pyautogui.click(92, 665)
+pyautogui.click(92, 675)
+pyautogui.click(92, 650)
+pyautogui.click(92, 640)
+pyautogui.click(92, 630)
+pyautogui.moveTo(92, 640)
+pyautogui.mouseDown()  # Pressiona o botão esquerdo do mouse
+pyautogui.mouseDown()  # Pressiona o botão esquerdo do mouse
+pyautogui.mouseDown()  # Pressiona o botão esquerdo do mouse
+pyautogui.mouseUp()    # Solta o botão esquerdo do mouse
+pyautogui.mouseUp()    # Solta o botão esquerdo do mouse
+pyautogui.mouseUp()    # Solta o botão esquerdo do mouse
+pyautogui.rightClick(92, 675)
+pyautogui.rightClick(92, 675)
+pyautogui.click(button="right")
+pyautogui.click(x=92, y=675, button="right")
+
